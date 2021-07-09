b6f53a5425e434e5cbe4964b6a1ea0b6a4106651
Surafel Gizachew <surameg@gmail.com>
2021-07-09 22:17:15 +0300

    created a bank Adaptor class

---
+++ bankAdaptor.java
@@ -0,0 +1,21 @@
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+public class bankAdaptor{
+    private static bankAdaptor instance = new bankAdaptor() ;
+
+    public static bankAdaptor getInstance() {
+        return instance ;
+    }
+
+    private String flag ;
+
+    private bankA A ;
+    private bankB B ;
+
+   
+   
+    
+}
