88876ecd0ee9d42aafa5061cca11dd53a3c74a7c
suraa <you@example.com>
2021-07-09 12:25:56 -0700

    bank added

---
+++ bank.java
@@ -0,0 +1,10 @@
+public interface Bank{
+
+    public void addBank(Account acc) ;
+   
+    public boolean authorizeATM(int card) ;
+    
+    public boolean authorizeCard(String passCode) ;
+
+    public double withdraw(String passCode,double amount) ;
+    
